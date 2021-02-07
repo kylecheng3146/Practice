33264504804ea8282cece5df5fdf6279a41f1026
Kyle Cheng <quickey123@gmail.com>
2021-02-07 12:07:01 +0800

    1672. Richest Customer Wealth

--- app/src/main/java/com/example/practice/LeetCode.kt
+++ app/src/main/java/com/example/practice/LeetCode.kt
@@ -1,6 +1,10 @@
 package com.example.practice
 
-import java.lang.StringBuilder
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 /**
  * Created by Kyle on 2/4/21.
@@ -24,5 +28,35 @@ class LeetCode {
             tempAddr.append(it).append("[.]")
         }
         return tempAddr.substring(0,tempAddr.length-3)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun simplifyPath(path: String): String {
+        val dirs = path.split("/")
+        val stack: Deque<String> = ArrayDeque()
+        for (s in dirs) {
+            if (s == "") continue
+            if (s == ".") continue
+            if (s == "..") stack.pollLast() else stack.add(s)
+        }
+        val result = "/" + java.lang.String.join("/", stack)
+        return result
+    }
+
+    /*
+    * 1672. Richest Customer Wealth
+    * */
+    fun maximumWealth(accounts: ArrayList<IntArray>): Int {
+        var result = 0
+        accounts.forEach {
+            var tempResult = 0
+            it.forEach {
+                tempResult += it
+            }
+            if(tempResult > result){
+                result = tempResult
+            }
+        }
+        return result
     }
 }
--- app/src/test/java/com/example/practice/LeetCodeTest.kt
+++ app/src/test/java/com/example/practice/LeetCodeTest.kt
@@ -7,17 +7,34 @@ import org.junit.Test
  */
 class LeetCodeTest {
 
+    val leetCode = LeetCode()
+
     @Test
     fun testRunningSum() {
         val nums: IntArray = intArrayOf(1,2,3,4)
-        val leetCode = LeetCode()
         leetCode.runningSum(nums)
     }
 
 
     @Test
     fun defangIPaddr() {
-        val leetCode = LeetCode()
         leetCode.defangIPaddr("1.1.1.1")
+    }
+
+    @Test
+    fun simplifyPath() {
+        leetCode.simplifyPath("/a/./b/../../c/")
+//        leetCode.simplifyPath("/home/")
+//        leetCode.simplifyPath("/../")
+//        leetCode.simplifyPath("/home//foo/")
+    }
+
+    @Test
+    fun maximumWealth(){
+        val accounts = arrayListOf<IntArray>()
+        accounts.add(intArrayOf(2,8,7))
+        accounts.add(intArrayOf(7,1,3))
+        accounts.add(intArrayOf(1,9,5))
+        leetCode.maximumWealth(accounts)
     }
 }
